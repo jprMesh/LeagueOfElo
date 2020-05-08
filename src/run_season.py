39fab20be80fc1605452f75a1d7fdbb48240826f
Jonas Rogers <jprmesh@gmail.com>
2020-05-08 16:44:49 -0700

    organized run_season script

--- src/run_season.py
+++ src/run_season.py
@@ -1,62 +1,65 @@
 from elo import elo
 from get_league_data import Leaguepedia_DB
 
+SEASON_RESET = None
+seasons = {
+    'LCS':[
+        "NA LCS 2018 Spring",
+        "NA LCS 2018 Spring Playoffs",
+        SEASON_RESET,
+        "NA LCS 2018 Summer",
+        "NA LCS 2018 Summer Playoffs",
+        SEASON_RESET,
+        "LCS 2019 Spring",
+        "LCS 2019 Spring Playoffs",
+        SEASON_RESET,
+        "LCS 2019 Summer",
+        "LCS 2019 Summer Playoffs",
+        SEASON_RESET,
+        "LCS 2020 Spring",
+        "LCS 2020 Spring Playoffs",
+        ],
+    'LEC':[
+        "EU LCS 2018 Spring",
+        "EU LCS 2018 Spring Playoffs",
+        SEASON_RESET,
+        "EU LCS 2018 Summer",
+        "EU LCS 2018 Summer Playoffs",
+        SEASON_RESET,
+        "LEC 2019 Spring",
+        "LEC 2019 Spring Playoffs",
+        SEASON_RESET,
+        "LEC 2019 Summer",
+        "LEC 2019 Summer Playoffs",
+        SEASON_RESET,
+        "LEC 2020 Spring",
+        "LEC 2020 Spring Playoffs",
+        ],
+    'LCK':[
+        "LCK 2019 Spring",
+        "LCK 2019 Spring Playoffs",
+        SEASON_RESET,
+        "LCK 2019 Summer",
+        "LCK 2019 Summer Playoffs",
+        SEASON_RESET,
+        "LCK 2020 Spring",
+        "LCK 2020 Spring Playoffs",
+        ]
+}
+
+teamfiles = {
+    'LCS': '../cfg/LCS_teams.csv',
+    'LEC': '../cfg/LEC_teams.csv',
+    'LCK': '../cfg/LCK_teams.csv'
+}
+
 def run_league(region):
+    season_list = seasons.get(region)
+    teamfile = teamfiles.get(region)
+    league = elo.EloRatingSystem(region, teamfile, K=30)
     lpdb = Leaguepedia_DB()
-    SEASON_RESET = None
 
-    if region == "LCS":
-        seasons = [
-            "NA LCS 2018 Spring",
-            "NA LCS 2018 Spring Playoffs",
-            SEASON_RESET,
-            "NA LCS 2018 Summer",
-            "NA LCS 2018 Summer Playoffs",
-            SEASON_RESET,
-            "LCS 2019 Spring",
-            "LCS 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LCS 2019 Summer",
-            "LCS 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LCS 2020 Spring",
-            "LCS 2020 Spring Playoffs",
-            ]
-        league = elo.EloRatingSystem(region, "../cfg/LCS_teams.csv", K=30)
-    elif region == "LEC":
-        seasons = [
-            "EU LCS 2018 Spring",
-            "EU LCS 2018 Spring Playoffs",
-            SEASON_RESET,
-            "EU LCS 2018 Summer",
-            "EU LCS 2018 Summer Playoffs",
-            SEASON_RESET,
-            "LEC 2019 Spring",
-            "LEC 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LEC 2019 Summer",
-            "LEC 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LEC 2020 Spring",
-            "LEC 2020 Spring Playoffs",
-            ]
-        league = elo.EloRatingSystem(region, "../cfg/LEC_teams.csv", K=30)
-    elif region == "LCK":
-        seasons = [
-            "LCK 2019 Spring",
-            "LCK 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LCK 2019 Summer",
-            "LCK 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LCK 2020 Spring",
-            "LCK 2020 Spring Playoffs",
-            ]
-        league = elo.EloRatingSystem(region, "../cfg/LCK_teams.csv", K=30)
-    else:
-        exit()
-
-    for season in seasons:
+    for season in season_list:
         if season:
             results = lpdb.get_season_results(season)
             league.loadGames(results, "Playoffs" in season)
@@ -65,61 +68,11 @@ def run_league(region):
     league.printStats()
 
 def run_pleague(region):
+    season_list = seasons.get(region)
+    teamfile = teamfiles.get(region)
+    league = elo.EloRatingSystem(region, teamfile, K=30)
     lpdb = Leaguepedia_DB()
-    SEASON_RESET = None
 
-    if region == "LCS":
-        seasons = [
-            "NA LCS 2018 Spring",
-            "NA LCS 2018 Spring Playoffs",
-            SEASON_RESET,
-            "NA LCS 2018 Summer",
-            "NA LCS 2018 Summer Playoffs",
-            SEASON_RESET,
-            "LCS 2019 Spring",
-            "LCS 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LCS 2019 Summer",
-            "LCS 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LCS 2020 Spring",
-            "LCS 2020 Spring Playoffs",
-            ]
-        teamfile = "../cfg/LCS_teams.csv"
-    elif region == "LEC":
-        seasons = [
-            "EU LCS 2018 Spring",
-            "EU LCS 2018 Spring Playoffs",
-            SEASON_RESET,
-            "EU LCS 2018 Summer",
-            "EU LCS 2018 Summer Playoffs",
-            SEASON_RESET,
-            "LEC 2019 Spring",
-            "LEC 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LEC 2019 Summer",
-            "LEC 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LEC 2020 Spring",
-            "LEC 2020 Spring Playoffs",
-            ]
-        teamfile = "../cfg/LEC_teams.csv"
-    elif region == "LCK":
-        seasons = [
-            "LCK 2019 Spring",
-            "LCK 2019 Spring Playoffs",
-            SEASON_RESET,
-            "LCK 2019 Summer",
-            "LCK 2019 Summer Playoffs",
-            SEASON_RESET,
-            "LCK 2020 Spring",
-            "LCK 2020 Spring Playoffs",
-            ]
-        teamfile = "../cfg/LCK_teams.csv"
-    else:
-        exit()
-
-    league = elo.PlayerEloRatingSystem(region, teamfile, K=30)
     for season in seasons:
         if season:
             league.loadRosters(lpdb.getSeasonRosters(season))
@@ -128,6 +81,5 @@ def run_pleague(region):
         else:
             league.newSeasonReset()
     league.printStats()
-
 
 run_league("LCK")
